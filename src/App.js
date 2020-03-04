57c50e1e9ae8b4df9e4044b8728c5e8a91f0e365
dayana-sog <dayanasog23@gmail.com>
2020-03-04 19:06:56 +0000

    Preparando a estilização global

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
+import Routes from './routes';
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <>
+      <Routes />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
